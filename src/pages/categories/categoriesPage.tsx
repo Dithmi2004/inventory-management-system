45b0bb8b24e4ca8e338bb4af173c128562375e76
Dithmi2004 <dithmiprameesha@gmail.com>
2026-07-24 20:23:28 +0530

    feat: create application layout and routing

---
+++ src/pages/categories/categoriesPage.tsx
@@ -0,0 +1,10 @@
+const CategoriesPage = () => {
+  return (
+    <div>
+      <h1>Categories</h1>
+      <p>Category management will appear here.</p>
+    </div>
+  );
+};
+
+export default CategoriesPage;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import CategoriesPage from "../pages/categories/categoriesPage";
+import DashboardPage from "../pages/dashboard/dashboardPage";
+import ProductsPage from "../pages/products/productsPage";
+import AppLayout from "../components/layout/appLayout";
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route element={<AppLayout />}></Route>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
